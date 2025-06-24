767e747d6accf4819fe7d177bb7b5ec8feca2082
IncDrops <graphics@incdrops.com>
2025-06-24 15:31:40 -0700

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PollFeed from "./components/PollFeed";
 import NewPollModal from "./components/NewPollModal";
@@ -6,26 +6,38 @@ import PollDetailView from "./components/PollDetailView";
 import SecondOpinionDetailView from "./components/SecondOpinionDetailView";
 
 const App = () => {
-  const [polls, setPolls] = useState([]);
-  const [showModal, setShowModal] = useState(false);
-
-  const handleCreatePoll = (newPoll) => {
-    setPolls([newPoll, ...polls]);
-  };
+  const [polls, setPolls] = React.useState([]);
+  const [showModal, setShowModal] = React.useState(false);
 
   return (
     <Router>
-      <div style={{ padding: "1rem" }}>
-        <button onClick={() => setShowModal(true)}>+ New Poll</button>
-        {showModal && (
-          <NewPollModal onClose={() => setShowModal(false)} onCreatePoll={handleCreatePoll} />
-        )}
-        <Routes>
-          <Route path="/" element={<PollFeed polls={polls} />} />
-          <Route path="/poll/:id" element={<PollDetailView polls={polls} />} />
-          <Route path="/opinion/:id" element={<SecondOpinionDetailView polls={polls} />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <button
+                onClick={() => setShowModal(true)}
+                style={{ marginTop: "2rem" }}
+              >
+                Create New Poll
+              </button>
+              <PollFeed polls={polls} />
+              {showModal && (
+                <NewPollModal
+                  onClose={() => setShowModal(false)}
+                  addPoll={(poll) => setPolls([...polls, poll])}
+                />
+              )}
+            </>
+          }
+        />
+        <Route path="/poll/:id" element={<PollDetailView polls={polls} />} />
+        <Route
+          path="/second-opinion/:id"
+          element={<SecondOpinionDetailView polls={polls} />}
+        />
+      </Routes>
     </Router>
   );
 };
